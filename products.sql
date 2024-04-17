f42ebc71211de7646f23032d5366a93630800dd7
lukxnut <kristian2015ok@gmail.com>
2024-04-17 10:32:00 +0700

    Add: Select Data

---
+++ products.sql
@@ -0,0 +1,44 @@
+create table products (
+    id varchar(10) not null,
+    name varchar(100) not null,
+    description text,
+    price int unsigned not null,
+    quantity int unsigned not null default 0,
+    created_at timestamp not null default current_timestamp,
+) engine = InnoDB;
+
+show tables;
+
+desc products;
+
+insert into
+    products(id, name, description, price, quantity)
+values
+    (
+        "P001",
+        "Mie Ayam Bakso",
+        "Mie Ayam Original + Bakso",
+        20000,
+        100
+    );
+
+select
+    *
+from
+    products;
+
+insert into
+    products(id, name, price, quantity)
+values
+    ("P002", "Mie Ayam Ceker", 16000, 100),
+    ("P003", "Mie Ayam Spesial", 25000, 100),
+    ("P004", "Mie Ayam Yamin", 16000, 100);
+
+select
+    id,
+    name,
+    price,
+    quantity
+from
+    products;
+
