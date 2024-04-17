552cff4d36831b68770d39a2a300382254566c29
lukxnut <kristian2015ok@gmail.com>
2024-04-17 11:52:27 +0700

    Add: Numeric Function

--- products.sql
+++ products.sql
@@ -340,3 +340,33 @@ select
     distinct category
 from
     products;
+
+select
+    10,
+    10,
+    10 * 10 as hasil;
+
+select
+    id,
+    name,
+    price,
+    price div 1000 as "Price in K"
+from
+    products;
+
+select
+    id,
+    cos(price),
+    sin(price),
+    tan(price)
+from
+    products;
+
+select
+    id,
+    name,
+    price
+from
+    products
+where
+    price div 1000 > 15;
