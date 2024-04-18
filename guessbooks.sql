dda69f5d89d2726d8e0dd656efb2a58c49c9a846
lukxnut <kristian2015ok@gmail.com>
2024-04-18 10:51:17 +0700

    Add: Set Operator

---
+++ guessbooks.sql
@@ -0,0 +1,99 @@
+create table guestbooks (
+    id int not null auto_increment,
+    email varchar(100),
+    title varchar(200),
+    content text,
+    primary key (id)
+) engine = InnoDB;
+
+select
+    *
+from
+    customers;
+
+select
+    *
+from
+    guestbooks;
+
+insert into
+    guestbooks(email, title, content)
+values
+    ("guest@gmail.com", "Hello", "Hello"),
+    ("guest2@gmail.com", "Hello", "Hello"),
+    ("guest3@gmail.com", "Hello", "Hello"),
+    ("tyn@gmail.com", "Hello", "Hello"),
+    ("tyn@gmail.com", "Hello", "Hello"),
+    ("tyn@gmail.com", "Hello", "Hello");
+
+select
+    *
+from
+    guestbooks;
+
+select
+    distinct email
+from
+    customers
+union
+select
+    distinct email
+from
+    guestbooks;
+
+select
+    email
+from
+    customers
+union
+all
+select
+    email
+from
+    guestbooks;
+
+select
+    emails.email,
+    count(emails.email)
+from
+    (
+        select
+            email
+        from
+            customers
+        union
+        all
+        select
+            email
+        from
+            guestbooks
+    ) as emails
+group by
+    emails.email;
+
+select
+    distinct email
+from
+    customers
+where
+    email in (
+        select
+            distinct email
+        from
+            guestbooks
+    );
+
+select
+    distinct customers.email
+from
+    customers
+    inner join guestbooks on (guestbooks.email = customers.email);
+
+select
+    distinct guestbooks.email,
+    customers.email
+from
+    customers
+    left join guestbooks on (guestbooks.email = customers.email)
+where
+    guestbooks.email is null;
