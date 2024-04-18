2632614a2e86f1b3da14b0769f51d326729cad8d
lukxnut <kristian2015ok@gmail.com>
2024-04-18 08:20:40 +0700

    Add: Join

--- wishlist.sql
+++ wishlist.sql
@@ -30,3 +30,55 @@ delete from
     products
 where
     id = "Pxxx";
+
+insert into
+    wishlist(id_product, description)
+values
+    ("P001", "Makanan Kesukaan");
+
+select
+    *
+from
+    wishlist
+    join products on (wishlist.id_product = products.id);
+
+select
+    w.id,
+    p.id,
+    p.name,
+    w.description
+from
+    wishlist as w
+    join products as p on (w.id_product = p.id);
+
+alter table
+    wishlist
+add
+    column id_customer int;
+
+alter table
+    wishlist
+add
+    constraint fk_wishlist_customer foreign key (id_customer) references customers(id);
+
+select
+    *
+from
+    customers;
+
+update
+    wishlist
+set
+    id_customer = 1
+where
+    id = 2;
+
+select
+    customers.email,
+    products.id,
+    products.name,
+    wishlist.description
+from
+    wishlist
+    join products on (products.id = wishlist.id_product)
+    join customers on (customers.id = wishlist.id_customer);
